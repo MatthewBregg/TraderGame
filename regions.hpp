57720ba94aa063c71db4de87ee9f0db57dab9a4f
Matthew Bregg <matthewbregg610@ufl.edu>
2014-08-24 16:22:31 -0400

    Regions

--- regions.hpp
+++ regions.hpp
@@ -9,7 +9,10 @@ class region
 public:
   region(Faction* owner= 0, Farm* f = nullptr, WoodMill* m = nullptr, Mine* mi = nullptr, City* c = nullptr, TraderCentre* tc = nullptr):farm(f),mill(m),mine(mi),city(c),tradeCentre(tc)
   {
-    owners.push_back(owner);
+    if(!owner)
+      {
+	owners.push_back(owner);
+      }
   };
 private:
   Farm* farm;
